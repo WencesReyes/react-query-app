f55bfba73eaff6afd5458effc7369a87a76370ec
wenceslao reyes <wenceslao.reyes@msw.com.mx>
2022-12-21 21:56:35 -0600

    sending a parameter to http service

--- src/pages/RQSuperheroes.tsx
+++ src/pages/RQSuperheroes.tsx
@@ -1,8 +1,25 @@
-import { useQuery } from "react-query";
+import { QueryFunctionContext, useQuery } from "react-query";
 import axios from "axios";
+import { useState } from "react";
+
+const getPost = async (params: QueryFunctionContext<(string | number)[], number>) => {
+  const [, postId] = params.queryKey;
+  
+  const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  return data;
+}
 
 const RQSuperheroes = () => {
-  const { data, isLoading } = useQuery('super-heroes', () => axios.get('http://localhost:4000/superheroes'))
+  const [postId, setPostId] = useState<number>(1);
+  const { data, isLoading } = useQuery(
+    ['post', postId],
+     getPost,
+     {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+     })
+
+  console.log({ data, isLoading });
 
   if (isLoading) {
     return <h2>Loading...</h2>
@@ -10,10 +27,12 @@ const RQSuperheroes = () => {
 
   return (
     <>
-      <h2>Super Heroes Page</h2>
-      {data?.data.map((hero: any) => {
-        return <div>{hero.name}</div>
-      })}
+      <h2>Post</h2>
+      <div>{data?.title}</div>
+      {/* {data?.data.map((hero: any) => {
+        return <div key={hero.name}>{hero.name}</div>
+      })} */}
+      <input type='number' value={postId} onChange={({ target }) => setPostId(+target.value)} />
     </>
   )
 }
